61a8df8875eb4d123a2875beb82a06b52df5f876
Fernandosfc07 <123068112+Fernandosfc07@users.noreply.github.com>
2023-09-05 10:42:39 +0200

    Add files via upload

---
+++ PildoraLiveActivities/PildoraLiveActivities/PildoraLiveActivitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PildoraLiveActivitiesApp.swift
+//  PildoraLiveActivities
+//
+//  Created by fernando.troncoso on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct PildoraLiveActivitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
